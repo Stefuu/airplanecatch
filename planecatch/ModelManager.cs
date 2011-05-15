4cebfef316ed537831c85d1be5f07cddaecd9423
Mauricio Verardo <mauricio.verardo@gmail.com>
2011-05-14 23:19:57 -0300

    teste com texturas

---
+++ planecatch/ModelManager.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+
+namespace planecatch
+{
+    /// <summary>
+    /// This is a game component that implements IUpdateable.
+    /// </summary>
+    public class ModelManager : DrawableGameComponent
+    {
+        List<BasicModel> _models = new List<BasicModel>();
+
+        public ModelManager(Game game)
+            : base(game)
+        {
+            // TODO: Construct any child components here
+        }
+
+        /// <summary>
+        /// Allows the game component to perform any initialization it needs to before starting
+        /// to run.  This is where it can query for any required services and load content.
+        /// </summary>
+        public override void Initialize()
+        {
+            // TODO: Add your initialization code here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _models.Add(new BasicModel(Game.Content.Load<Model>("Models\\TexturedCube")));
+
+            base.LoadContent();
+        }
+
+        /// <summary>
+        /// Allows the game component to update itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        public override void Update(GameTime gameTime)
+        {
+            foreach (var basicModel in _models)
+            {
+                basicModel.Update();   
+            }
+
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            foreach (var basicModel in _models)
+            {
+                basicModel.Draw(((PlaneCatchGame) Game).Camera);
+            }
+
+            base.Draw(gameTime);
+        }
+    }
+}
