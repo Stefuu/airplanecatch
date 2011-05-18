1092a02e715b773b3c65905351feb0303558eae0
Mauricio Verardo <mauricio.verardo@gmail.com>
2011-05-18 00:22:51 -0300

    - Alterada a classe InputHelper para ser um componente do jogo, e assim ter acesso ? deferentes momentos. - Alterada a classe Camera para permitir que o jogador pule quando teclar espa?o.

--- planecatch/Camera.cs
+++ planecatch/Camera.cs
@@ -15,37 +15,48 @@ namespace planecatch
         public Vector3 Position { get; set; }
         public IPlayerState CurrentState { get; set; }
 
-
-        private Vector3 Direction { get; set; }
+        private Vector3 TargetDirection { get; set; }
         private Vector3 Up { get; set; }
         private Vector3 SideVector
         {
             //Para andar de lado, basta encontrar o produto vetorial entre o vetor Up e o vetor que aponta a direção do target.
             get { return Vector3.Cross(Up, WalkingDirection); }
         }
+
         private Vector3 WalkingDirection
         {
             //Para o jogador não ficar voando, é necessário remover todo o valor y da direção pra onde ele vai andar.
             //Para que a velocidade dele não caia, é preciso adicionar no x o que foi tirado do y;
-            get { return Direction + new Vector3(Direction.Y, -Direction.Y, 0); }
+            get { return TargetDirection + new Vector3(TargetDirection.Y, -TargetDirection.Y, 0); }
         }
+
+        private Vector3 Velocity { get; set; }
 
         private MouseState _previousMouseState;
         private IPlayerState _walkingState;
         private IPlayerState _runningState;
+        private bool _jumping;
+        private readonly Vector3 _gravityAccel = new Vector3(0, 0.7f, 0);
         
         public Camera(Game game, Vector3 position, Vector3 target, Vector3 up) : base(game)
         {
             Position = position;
-            Direction = target - position;
-            Direction = Vector3.Normalize(Direction);
+            TargetDirection = target - position;
+            TargetDirection = Vector3.Normalize(TargetDirection);
             Up = up;
+
+            InitializePhysics();
 
             InitializeStates();
 
             CreateLookAt();
 
             InitializeProjection(game);
+        }
+
+        private void InitializePhysics()
+        {
+            Velocity = Vector3.Zero;
         }
 
         private void InitializeProjection(Game game)
@@ -62,53 +73,91 @@ namespace planecatch
             _runningState = new Running();
         }
 
-        public override void Initialize()
-        {
-            base.Initialize();
-
-            ResetMouse();
-
-            _previousMouseState = Mouse.GetState();
-        }
-
         private void CreateLookAt()
         {
-            var target = Position + Direction;
+            var target = Position + TargetDirection;
             View = Matrix.CreateLookAt(Position, target, Up);
-        }
-
-        public override void Update(GameTime gameTime)
-        {
-            //Se algum shift estiver pressionado, o jogador está correndo.
-            HandleInput();
-
-            Yaw();
-            Pitch();
-            
-            CreateLookAt();
-
-            ResetMouse();
-
-            base.Update(gameTime);
         }
 
         private void HandleInput()
         {
-            InputHelper.ExecuteIf(
+            var helper = ((PlaneCatchGame) Game).InputHelper;
+
+            helper.ExecuteIf(
                 (s) => s.IsKeyDown(Keys.LeftShift) || s.IsKeyDown(Keys.RightShift),
                 ToRunningState
                 );
             
             //Se os dois shifts estiverem soltos, o jogador está andando.
-            InputHelper.ExecuteIf(
+            helper.ExecuteIf(
                 (s) => s.IsKeyUp(Keys.LeftShift) && s.IsKeyUp(Keys.RightShift),
                 ToWalkingState
                 );
 
-            InputHelper.ExecuteIfKeyPressed(Keys.W, () => Position += WalkingDirection * CurrentState.Speed);
-            InputHelper.ExecuteIfKeyPressed(Keys.S, () => Position -= WalkingDirection * CurrentState.Speed);
-            InputHelper.ExecuteIfKeyPressed(Keys.A, () => Position += SideVector * CurrentState.Speed);
-            InputHelper.ExecuteIfKeyPressed(Keys.D, () => Position -= SideVector * CurrentState.Speed);
+            helper.ExecuteIfKeyPressed(Keys.W, Forward);
+            helper.ExecuteIfKeyPressed(Keys.S, Backwards);
+            helper.ExecuteIfKeyPressed(Keys.A, StrafeLeft);
+            helper.ExecuteIfKeyPressed(Keys.D, StrafeRight);
+            helper.ExecuteIfJustPressed(Keys.Space, Jump);
+
+            helper.ExecuteIf(
+                s => 
+                    s.IsKeyUp(Keys.W) && 
+                    s.IsKeyUp(Keys.A) && 
+                    s.IsKeyUp(Keys.S) && 
+                    s.IsKeyUp(Keys.D)
+                , Stand
+                );
+        }
+
+        private void Jump()
+        {
+            Velocity += new Vector3(0, 10, 0);
+            _jumping = true;
+        }
+
+        private void StrafeRight()
+        {
+            Velocity += -(SideVector * CurrentState.Speed);
+        }
+
+        private void StrafeLeft()
+        {
+            Velocity += (SideVector * CurrentState.Speed);
+        }
+
+        private void Backwards()
+        {
+            Velocity += -(WalkingDirection * CurrentState.Speed);
+        }
+
+        private void Forward()
+        {
+            Velocity += WalkingDirection * CurrentState.Speed;
+        }
+
+        private void Stand()
+        {
+            ResetVelocity();
+        }
+
+        private void ResetVelocity()
+        {
+            Velocity -= new Vector3(Velocity.X, 0, Velocity.Z);
+        }
+
+        private void UpdatePosition()
+        {
+            Position += Velocity;
+            ResetVelocity();
+        }
+
+        private void ApplyGravity()
+        {
+            if(_jumping)
+            {
+                Velocity -= _gravityAccel;
+            }
         }
 
         private void ToRunningState()
@@ -125,8 +174,8 @@ namespace planecatch
         {
             var angle = (MathHelper.PiOver4 / 150) * (Mouse.GetState().Y - _previousMouseState.Y);
 
-            Direction = Vector3.Transform(Direction,
-                                          Matrix.CreateFromAxisAngle(Vector3.Cross(Up, Direction) * CurrentState.Speed,
+            TargetDirection = Vector3.Transform(TargetDirection,
+                                          Matrix.CreateFromAxisAngle(Vector3.Cross(Up, TargetDirection) * CurrentState.Speed,
                                                                     angle));
         }
 
@@ -134,7 +183,7 @@ namespace planecatch
         {
             var angle = (-MathHelper.PiOver4 / 150) * (Mouse.GetState().X - _previousMouseState.X);
 
-            Direction = Vector3.Transform(Direction,
+            TargetDirection = Vector3.Transform(TargetDirection,
                                           Matrix.CreateFromAxisAngle(Up,
                                                                     angle));
         }
@@ -145,6 +194,44 @@ namespace planecatch
             var middleHeight = Game.Window.ClientBounds.Height / 2;
 
             Mouse.SetPosition(middleWidth, middleHeight);
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            ResetMouse();
+
+            _previousMouseState = Mouse.GetState();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            HandleInput();
+
+            ApplyGravity();
+
+            UpdatePosition();
+
+            CheckCollision();
+
+            Yaw();
+            Pitch();
+
+            CreateLookAt();
+
+            ResetMouse();
+
+            base.Update(gameTime);
+        }
+
+        private void CheckCollision()
+        {
+            if (Position.Y > 0) return;
+
+            Velocity = new Vector3(Velocity.X, 0, Velocity.Z);
+            Position = new Vector3(Position.X, 0, Position.Z);
+            _jumping = false;
         }
     }
 }
